3970242ba9c5c13f1f5c61dad3fba10c3df9efcb
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2022-11-30 17:43:11 -0300

    fit: renderizando base de dados

--- src/app/views/carousel/carousel/carousel.component.ts
+++ src/app/views/carousel/carousel/carousel.component.ts
@@ -1,41 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { transition, trigger, useAnimation } from '@angular/animations';
 import { ImdbService } from 'src/app/services/imdb.service';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
+  // @Input() animationType = CarouselAnimation.Fade;
 
-  movies: any = []
-  constructor(private imdbService: ImdbService) { }
+  public movies: any = [];
+  currentMovie = 0;
 
-  ngOnInit() {
-    this.getMovies()
+  constructor(private imdb: ImdbService) {}
+
+  ngOnInit(): void {
+    this.getMovies();
   }
 
-  getMovies() {
-    this.imdbService.getData().subscribe((data) => {
-      data.forEach(item => {
-        console.log('items', item)
-        this.movies.push(item)
-        this.movies.push
-        console.log('this.movies', this.movies)
+  getMovies(): void {
+    this.imdb.getData().subscribe((data: any) => {
+      data.forEach((item: any) => {
+        this.movies.push(item);
         while (this.movies.length > 10) {
-          this.movies.pop()
+          this.movies.pop();
         }
         return;
       });
-      // this.movies.forEach((movie: any) => {
-      //   this.imdbService.getPosters(movie.id).subscribe((data) => {
-      //     // console.log('data', data)
-      //     movie.image = data.posters[0].link;
-      //     this.imdbService.putPosters(movie.id, movie);
-      //     return;
-      //   });
-      // });
+      /*       this.movies.forEach((movie: any) => {
+        this.imdb.getPosters(movie.id).subscribe((data) => {
+          console.log(data);
+          movie.image = data.posters[0].link;
+          this.imdb.putPosters(movie.id, movie);
+          return;
+        });
+      });
+
+      console.log(this.movies); */
     });
-    console.log('image', this.movies)
+  }
+
+  onPreviousClick() {
+    const previous = this.currentMovie - 1;
+    this.currentMovie = previous < 0 ? this.movies.length - 1 : previous;
+  }
+
+  onNextClick() {
+    const next = this.currentMovie + 1;
+    this.currentMovie = next === this.movies.length ? 0 : next;
   }
 }
